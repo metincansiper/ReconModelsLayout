372317bf96082fe840b37196234d920dcdcf3084
metincansiper <metincansiper@gmail.com>
2018-04-01 23:57:57 -0700

    Parse MODEL_IDS_START and MODEL_IDS_END to int in config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const USE_STORED_RGD = true; // Use recon graph data stored in xml folder
 const SAVE_QUERIED_RGD = true; // Store queried recon graph data to xml folder
 const DEFAULT_MODEL_IDS_PATH = 'input/modelIDs.csv';
 const MODEL_IDS_PATH = process.env.MODEL_IDS_PATH || DEFAULT_MODEL_IDS_PATH;
-const MODEL_IDS_START = process.env.MODEL_IDS_START;
-const MODEL_IDS_END = process.env.MODEL_IDS_END;
+const MODEL_IDS_START = parseInt(process.env.MODEL_IDS_START);
+const MODEL_IDS_END = parseInt(process.env.MODEL_IDS_END);
 
 module.exports = { LAYOUT_NAME, USE_STORED_RGD, SAVE_QUERIED_RGD, MODEL_IDS_PATH, MODEL_IDS_START, MODEL_IDS_END };
